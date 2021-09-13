9b75a3c05b9b0a178fe59548aad6093813a0518e
Renato <rlbarrae@hotmail.com>
2021-09-13 09:14:35 +0200

    refactored

--- app/src/main/java/com/chetocoders/chetogames/ui/di.kt
+++ app/src/main/java/com/chetocoders/chetogames/ui/di.kt
@@ -7,3 +7,7 @@ import dagger.hilt.android.components.ViewModelComponent
 @Module
 @InstallIn(ViewModelComponent::class)
 class SplashFragmentModule {}
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class GameCatalogFragment {}
---
+++ app/src/main/java/com/chetocoders/chetogames/ui/gameCatalog/GameCatalogViewModel.java
@@ -0,0 +1,9 @@
+package com.chetocoders.chetogames.ui.gameCatalog;
+
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
+class GameCatalogViewModel  @Inject GameCatalogViewModel() : ViewModel() {}
