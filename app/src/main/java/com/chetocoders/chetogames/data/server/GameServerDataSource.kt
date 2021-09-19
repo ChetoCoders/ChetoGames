f226bc63f38c9b698ea14a723e55ec698758b248
Renato <rlbarrae@hotmail.com>
2021-09-19 12:26:26 +0200

    Change request body

--- app/src/main/java/com/chetocoders/chetogames/data/server/GameServerDataSource.kt
+++ app/src/main/java/com/chetocoders/chetogames/data/server/GameServerDataSource.kt
@@ -3,11 +3,21 @@ package com.chetocoders.chetogames.data.server
 import com.chetocoders.chetogames.data.server.mapper.toDomain
 import com.chetocoders.domain.GameDetail
 import com.chetocoders.source.RemoteDataSource
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.internal.filterList
 
 class GameServerDataSource : RemoteDataSource {
 
     override suspend fun getGames(): List<GameDetail> =
         GamesServer.service
-            .getGames(ApiQueryConstants.GAME_DETAILS_QUERY).map { it.toDomain() }
+            .getGames(
+                ApiQueryConstants.GAME_DETAILS_QUERY
+                    .toRequestBody("text/plain".toMediaTypeOrNull())
+            )
+            .map {
+                it.toDomain() }
 
 }
--- app/src/main/java/com/chetocoders/chetogames/data/server/GamesServerService.kt
+++ app/src/main/java/com/chetocoders/chetogames/data/server/GamesServerService.kt
@@ -1,12 +1,15 @@
 package com.chetocoders.chetogames.data.server
 
 import com.chetocoders.chetogames.data.server.dto.GameDTO
+import okhttp3.RequestBody
 import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface GamesServerService {
 
     @POST("games")
-    suspend fun getGames(@Body query: String): List<GameDTO>
+    suspend fun getGames(@Body query: RequestBody): List<GameDTO>
 
 }
