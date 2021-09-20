8d8f956ad69b80f8f526aea6d597c072f306284e
Juan Cordero Ordóñez <juancorderoordonez@gmail.com>
2021-09-20 23:54:01 +0200

    Fixed build project

--- app/src/main/java/com/chetocoders/chetogames/ui/di.kt
+++ app/src/main/java/com/chetocoders/chetogames/ui/di.kt
@@ -2,19 +2,10 @@ package com.chetocoders.chetogames.ui
 
 
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
-import com.chetocoders.data.repository.GameRepository
-import com.chetocoders.usecases.GetGames
-
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class GameCatalogFragment {
-    @Provides
-    @ViewModelScoped
-    fun getGamesProvider(gameRepository: GameRepository) =
-        GetGames(gameRepository)
 }
