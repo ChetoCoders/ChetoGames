3b510e3a0e8f4ef854e5abf2135cffda3b9ec609
Renato <rlbarrae@hotmail.com>
2021-09-20 19:58:48 +0200

    Refactor imports in app module

--- app/src/main/java/com/chetocoders/chetogames/di/AppModule.kt
+++ app/src/main/java/com/chetocoders/chetogames/di/AppModule.kt
@@ -1,17 +1,12 @@
 package com.chetocoders.chetogames.di
 
 
-import android.content.Context
-import androidx.room.Room
-import com.chetocoders.chetogames.data.database.GameDatabase
 import com.chetocoders.chetogames.data.server.GameServerDataSource
+import com.chetocoders.data.source.RemoteDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import com.chetocoders.data.source.RemoteDataSource
-import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
